caf504bc50ff1f6250f3a4de3eb411a71c7818dd
enarsinick <nxdmedia@googlemail.com>
2021-12-13 11:39:25 +0000

    REM38 #comment API now returns all notes from DB

--- api/remind-api/src/main/java/com/nickcave/remind/service/NoteService.java
+++ api/remind-api/src/main/java/com/nickcave/remind/service/NoteService.java
@@ -1,9 +1,13 @@
 package com.nickcave.remind.service;
 
+import java.util.List;
+
 import com.nickcave.remind.domain.Note;
 
 public interface NoteService {
 	
 	public Note createNote(Note note);
-
+	
+	public List<Note> getAll();
+	
 }
